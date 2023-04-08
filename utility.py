065994b0110fdf20e0274c728c8b3924fa9041d2
CSharpProjector <marleymulvinbroome@gmail.com>
2023-04-08 19:15:10 +1000

    Implemented audio and visual duration getting util functions

--- utility.py
+++ utility.py
@@ -1,15 +1,30 @@
-import re
-from requests import get
+from moviepy.video.io.VideoFileClip import VideoFileClip
 
 
 def remove_links_from_text(text: str) -> str:
+    import re
+
     text = re.sub(r'http\S+', '', text)
     text = re.sub(r'www\S+', '', text)
     return text
 
 
 def download_img(url, destination) -> None:
+    from requests import get
+
     img_data = get(url).content
 
     with open(destination, 'wb') as handler:
         handler.write(img_data)
+
+
+def get_audio_duration(audio_path: str) -> float:
+    from mutagen.mp3 import MP3
+
+    audio = MP3(audio_path)
+    return audio.info.length
+
+
+def get_video_duration(video_path: str) -> float:
+    video = VideoFileClip(video_path)
+    return video.duration
